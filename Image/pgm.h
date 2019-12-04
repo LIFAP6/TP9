6aaaf177fdfdcf2cfea91f3ddc213559eb5cfde4
Quentin CHAPEL <chapelquen@gmail.com>
2019-12-04 16:49:18 +0100

    [Feat]PGM Image
    Adding function about loading with all related functions

---
+++ Image/pgm.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#ifndef PGMIMAGE
+#define PGMIMAGE
+    class PGMImage{
+        private:
+            vector<int> pgmArray;
+            int numCol;
+            int numRow;
+            int maxIntensity;
+            int imageBruit;
+
+        public:
+            //Constructeur
+            PGMImage();
+            PGMImage(string filePath);
+
+            //Destructeur
+            ~PGMImage();
+
+            //Surcharge d'opérateur
+            PGMImage &operator=(const PGMImage &node);
+            //ostream &operator<<(std::ostream &os);
+
+            //Import/export de l'image
+            void loadImage(string filePath);
+            void saveTo(string filePath);
+
+            //Accès à un élément
+            int getElementNord(int i, int j);
+            int getElementSud(int i, int j);
+            int getElementOuest(int i, int j);
+            int getElementEst(int i, int j);
+
+            //Modification de l'intensité d'un noeud
+            void updateNoeud(int i, int j, int value);
+
+            //Affichage
+            void displayImage();
+    };
+#endif
